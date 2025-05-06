44a65b2952ef872caafe53fbd67381e7a3f03fa4
WorldFS <fsworld009@gmail.com>
2025-05-06 16:48:17 +0800

    first commit: Minimum working chrome extension on local

---
+++ README.md
@@ -0,0 +1,14 @@
+# Development
+
+## Install
+
+```
+npm install -g pnpm@latest-10
+pnpm i --frozen-lockfile
+```
+
+
+# Ref
+
+* https://medium.com/@5tigerjelly/creating-a-chrome-extension-with-react-and-vite-boilerplate-provided-db3d14473bf6
+* https://dev.to/cassidoo/how-to-test-a-browser-extension-locally-4bne
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), viteStaticCopy({
+    targets: [
+      {
+        src: 'manifest.json',
+        dest: '.',
+      }
+    ],
+  }),],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
+})
